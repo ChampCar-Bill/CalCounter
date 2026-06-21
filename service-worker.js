4bce5af96b5856f708f58ab42cf9ae0e79b5716f
William Strong <161987009+ChampCar-Bill@users.noreply.github.com>
2026-06-21 19:27:24 -0400

    Update service worker cache version and add files

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = "calorie-counter-v1";
+const CACHE_NAME = "calorie-counter-v4";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./service-worker.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
